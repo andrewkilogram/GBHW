c15184b2d3d91a6700fdc43775fdfff289916453
andrewkilogram <110405641+andrewkilogram@users.noreply.github.com>
2022-08-08 21:17:57 +0900

    Delete урк2_з4.py

--- урк2_з4.py
+++
@@ -1,9 +0,0 @@
-lesson = 'Урок 2, задание 4 \n'
-
-print(lesson)
-print('Вывести каждое слово с новой строки')
-
-spisok = input("Введите слова через пробел: ").split()
-
-for i, spisok in enumerate(spisok):
-    print(i + 1, "-", "%.10s" % (spisok))
